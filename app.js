4b09aeea0029a509b29dd260e5b32a92bf0323fa
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-10-06 15:02:44 +0300

    separate html

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+function fetchComponent(url, attr = url) {
+    fetch(`./html/${url}.html`)
+        .then(res => res.text()
+            .then(block => document.querySelector(attr).innerHTML = block))
+}
+
+function ready() {
+    fetchComponent("header")
+    fetchComponent("container-top", "#container-top")
+    fetchComponent("aside")
+    fetchComponent("main")
+    fetchComponent("footer")
+}
+document.addEventListener("DOMContentLoaded", ready);
+
 // const hCard = document.querySelector(".")
-const card = document.createElement('div.card')
+// const card = document.createElement('div.card')
