96c702f5f5685cbe9a4806f179fc0dd3118a0b6c
Anton Hryhoriev <a.gryhoryev@gmail.com>
2022-10-11 22:41:35 +0300

    left to do double filter and icons

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-import pagination from './js/pagination.js'
 
 function fetchComponent(url, attr = url) {
   fetch(`./html/${url}.html`)
@@ -68,3 +67,93 @@ function getCards() {
 }
 const nPagination = document.querySelector('.pagination')
 nPagination.addEventListener("click", pagination)
+
+function pagination(e) {
+  const paginations = nPagination.querySelectorAll('.page-item')
+  const prev = nPagination.querySelector('.prev')
+  const next = nPagination.querySelector('.next')
+  const current = nPagination.querySelector('.active .page-link')
+
+  let prevCurrent = false
+  let nextCurrent = false
+
+  if (e.target == prev && (+current.textContent - 1 == 1 || current.textContent == 1)) {
+      prevCurrent = true
+  }
+
+  if (e.target == next && +current.textContent + 1 == paginations.length) {
+      nextCurrent = true
+  }
+
+  if (e.target.textContent == 1 || prevCurrent) {
+      prev.classList.add("disabled")
+      next.classList.remove("disabled")
+
+  } else if (e.target.textContent == paginations.length || nextCurrent) {
+      next.classList.add("disabled")
+      prev.classList.remove("disabled")
+  } else {
+      next.classList.remove("disabled")
+      prev.classList.remove("disabled")
+  }
+
+  if (e.target == prev) {
+      reset()
+      paginations.forEach(i => {
+          if (i.textContent == +current.textContent - 1) {
+              i.classList.add("active")
+          }
+      })
+  } else if (e.target == nPagination.firstElementChild) {
+      reset()
+      current.parentNode.classList.add('active')
+      prev.classList.add("disabled")
+  }
+
+  else if (e.target == next) {
+      reset()
+      paginations.forEach(i => {
+          if (i.textContent == +current.textContent + 1) {
+              i.classList.add("active")
+          }
+      })
+  } else if (e.target == nPagination.lastElementChild) {
+      reset()
+      current.parentNode.classList.add('active')
+      next.classList.add("disabled")
+  } else {
+      reset()
+      e.target.closest(".page-item").classList.add("active")
+  }
+  function reset() {
+      paginations.forEach(i => {
+          i.classList.remove("active")
+      })
+  }
+}
+
+
+// range slider
+
+const setLabel = (lab, value) => {
+  const label = document.querySelector(`#slider-${lab}-label`);
+  label.text(value);
+  const slider = document.querySelector(`#slider-div .${lab}-slider-handle`);
+  const rect = slider[0].getBoundingClientRect();
+  label.offset({
+    top: rect.top - 30,
+    left: rect.left
+  });
+}
+
+const setLabels = (values) => {
+  setLabel("min", values[0]);
+  setLabel("max", values[1]);
+}
+
+
+document.querySelector('#ex2').slider().addEventListener('slide', function(e) {
+  setLabels(e.value);
+});
+
+setLabels($('#ex2').attr("data-value").split(","));
